fd3f8a00f857376a35853976cd5cf1a234bc25a0
Ryan Ouasti <ryanouasti@ZiraPC.local>
2025-07-02 23:47:43 +0200

    module

---
+++ Infra/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.1.0"
+}
+
+provider "aws" {
+  region = "eu-west-3"
+}
+
+module "prometheus" {
+  source = "./Prometheus"
+
+}
+
+module "web" {
+  source = "./Serveur_web"
+  depends_on = [ module.prometheus ]
+}
+
+module "grafana" {
+  source = "./Grafana"
+  depends_on = [ module.prometheus ]
+}
